5270651b821f581dfe1446334e4079b4db6aeb1d
Rodolfo <rodolfo.dartora@jeaholding.com.br>
2022-10-10 09:42:12 -0300

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          <div className="title">Typescript CRUD</div>
+          <div className="subtitle"> With React and TypeORM</div>
+        </h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
