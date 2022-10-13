da7e32c349cb22991e10cd624bd2d51256ac8cf6
Rodolfo <rodolfo.dartora@jeaholding.com.br>
2022-10-13 08:59:16 -0300

    Fetch Callback_44:50

--- src/hooks/useMutation.tsx
+++ src/hooks/useMutation.tsx
@@ -3,19 +3,31 @@ import { Record } from '../interfaces/RecordEntities';
 
 export type Action<T> = (record: T) => Promise<void>;
 
-export const useMutation = <T extends Record>(path: string) => {
+export const useMutation = <T extends Record>(
+  path: string,
+  callback?: Function
+) => {
   const url = `${process.env.REACT_APP_API}/${path}`;
+  const wrap = (fn: Action<T>) => {
+    return async (record: T) => {
+      fn(record).then(() => {
+        if (callback) {
+          callback();
+        }
+      });
+    };
+  };
 
-  const create: Action<T> = async (record: T) => {
+  const create: Action<T> = wrap(async (record: T) => {
     await axios.post(url, record);
-  };
+  });
 
-  const update: Action<T> = async (record: T) => {
+  const update: Action<T> = wrap(async (record: T) => {
     await axios.put(`${url}/${record.id}`, record);
-  };
-  const remove: Action<T> = async (record: T) => {
+  });
+  const remove: Action<T> = wrap(async (record: T) => {
     await axios.delete(`${url}/${record.id}`);
-  };
+  });
 
   return {
     create,
